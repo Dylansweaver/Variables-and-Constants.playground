f4fd55ce545124edb117d7721086fb97fe74f7ef
Dylansweaver <dweaver@e112-m-15.mchenry.edu>
2016-09-06 16:37:57 -0500

    Variables and Constants

---
+++ Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var d3:Int = 5;
+var float = 7.2;
+var double = 3.1459;
+let x = 5;
+let y = 2.7;
+let z = 9.5413;
+let s1:String = "Chimichangas";
+let s2:Float = 42;
+let s3:Double = 1.98;
+
+let d1 = 54;
+
+d1 = 5;
+
+let rootBeer:Float = 4;
+print(rootBeer)//Experiment Question: it just puts a decimal after the 4.
